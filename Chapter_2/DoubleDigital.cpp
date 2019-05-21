aafd764e0bab4c4c31aa48551417881113080348
santial66 <s.anasagasti@gmail.com>
2019-05-21 23:37:44 +0200

    bug fixed

--- Chapter_2/DoubleDigital.cpp
+++ Chapter_2/DoubleDigital.cpp
@@ -9,7 +9,7 @@ double PayoffDoubleDigital::operator()(double spot) const
 {
 	if (spot <= _lowerLevel)
 		return 0.0;
-	if (spot >= _lowerLevel)
+	if (spot >= _upperLevel)
 		return 0.0;
 	return 1.0;
 
