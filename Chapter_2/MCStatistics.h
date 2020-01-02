fa1b2c17b24c74f60daa30d3873a35a8b86e941f
santial66 <s.anasagasti@gmail.com>
2020-01-02 17:08:06 +0100

    adding statistic gatherer

---
+++ Chapter_2/MCStatistics.h
@@ -0,0 +1,43 @@
+
+#ifndef STATISTICS_H
+#define STATISTICS_H
+
+#include <vector>
+
+class StatisticsMC
+{
+public:
+	StatisticsMC() {}
+
+	virtual void DumpOneResult(double result) = 0;
+	virtual std::vector<std::vector<double> > GetResultSoFar() const = 0;
+
+	virtual StatisticsMC* clone()const = 0;
+	virtual ~StatisticsMC() {}
+private:
+
+};
+
+class StatisticsMean : public StatisticsMC
+{
+public:
+	StatisticsMean();
+
+	virtual void DumpOneResult(double result);
+	virtual std::vector<std::vector<double> > GetResultSoFar() const;
+
+	virtual StatisticsMean* clone()const;
+	virtual ~StatisticsMean() {}
+private:
+	double _runningSum;
+	unsigned long _pathsDone;
+
+}
+
+
+
+
+#endif // !STATISTICS_H
+
+
+
